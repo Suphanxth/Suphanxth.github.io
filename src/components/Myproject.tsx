66b1484c40caa33074b1e545f7ee7d4ddcf69569
Suphanxth <suphanath.wangtip@gmail.com>
2025-04-25 23:52:32 +0700

    Add resume in resume page

--- src/components/Myproject.tsx
+++ src/components/Myproject.tsx
@@ -2,8 +2,8 @@ import './Myproject.css'
 
 function Myproject() {
   return (
-    <div>
-        <div className="min-h-screen flex justify-center items-start pt-4">
+    <div className="myproject">
+        <div className="container">
             <div className="box">  
                 <div className="inner">
                     <span>My Project</span>
--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,7 +1,19 @@
-function Resume(){
-    return(
-        <div>Resume แบบ เขียนเอาเอง</div>
-    )
+import './Resume.css';
+
+function Resume() {
+    return (
+        <div className='resume'>
+            <div className='resume__container'>
+                <iframe 
+                    src="https://drive.google.com/file/d/1U4bC-jb7roDT9iOXDXz221jhFZYERXI8/preview" 
+                    width="90%" 
+                    height="760" 
+                    allow="autoplay"
+                    title="resume-preview"
+                ></iframe>
+            </div>
+        </div>
+    );
 }
 
-export default Resume
+export default Resume;

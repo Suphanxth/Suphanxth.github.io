e4f1d8ec7950750072297c5378784bf0119458bd
Suphanxth <suphanath.wangtip@gmail.com>
2025-04-28 19:34:18 +0700

    Update resume, but in the near future, I plan to write my resume using HTML, TypeScript, or another programming language!!!!!

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,7 +5,7 @@ function Resume() {
         <div className='resume'>
             <div className='resume__container'>
                 <iframe 
-                    src="https://drive.google.com/file/d/1U4bC-jb7roDT9iOXDXz221jhFZYERXI8/preview" 
+                    src="https://drive.google.com/file/d/1eQydTDiNdnmVhwKKqNtEc24HTbJ0bE7t/preview" 
                     width="90%" 
                     height="760" 
                     allow="autoplay"
